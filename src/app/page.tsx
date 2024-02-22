9c8593ca3cd7476197e728f12d92ebc3cad1375c
CATAAA233 <carlosdanielcatalan1o@gmail.com>
2024-02-22 00:18:24 -0800

    Feat (Coverpage & fonts): Adding new Fonts and Coverpage Responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,24 @@
-import ServiceCard from '@/components/ServiceCard/indext'
+import ServiceCard from '@/components/ServiceCard'
 import styles from './page.module.css'
 import Image from 'next/image'
+import { DarkLayer } from '@/components'
 export default function Home () {
   return (
     <main className={styles.main}>
       <div className={styles.Coverpage}>
+        <DarkLayer/>
         <Image className={styles.CoverImage} src={'/images/coverpage.webp'} alt={'Coverpage Image'} fill />
         <h1>Find Yourself Outside.</h1>
         <p>Book unique camping experiences on over 300,000 campsites, cabins, RV parks, public parks and more.</p>
         <button className={styles.Button}>Discover</button>
       </div>
       <section className={styles.ServicesCards__Section}>
-        <div className={styles.ServicesCards__List}>
+        {/* <div className={styles.ServicesCards__List}>
           <ServiceCard iconSrc={'/icons/campfire.webp'} title={'Camping & Day Use'} description={'Return to your favorite spot or discover a new one that&aposs right for you'} />
           <ServiceCard iconSrc={'/icons/tickets.webp'} title={'Tours & Tickets'} description={'Reserve tours and tickets to participate in events'} />
           <ServiceCard iconSrc={'/icons/permit.webp'} title={'Permits'} description={'Obtain permits of access to high-demand locations'} />
           <ServiceCard iconSrc={'/icons/fishing.webp'} title={'Recreation Activities'} description={'Return to your favorite spot or discover a new one that&aposs right for you'} />
-        </div>
+        </div> */}
       </section>
     </main>
   )
---
+++ src/components/DarkLayer/index.tsx
@@ -0,0 +1,6 @@
+import styles from './DarkLayer.module.css'
+export default function DarkLayer(){
+    return(
+        <div className={styles.Container}/>
+    )
+}
